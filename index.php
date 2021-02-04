206c6351bc88b2bd902f96ae3b7cf5f1836ed8ba
Andra Bayu Febriant <febriantbayuandra@gmail.com>
2021-02-04 21:34:55 +0700

    Setup Environment Bootstrap
    Environment bootstrap sudah ready

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Siakad UPM</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+    
+
+<script src="jquery-3.5.1.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
